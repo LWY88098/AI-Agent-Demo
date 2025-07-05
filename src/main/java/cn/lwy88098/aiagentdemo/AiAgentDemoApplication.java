ea404b222d61619168f4a1285946c063b03de59b
LWY88098 <lwy88098@gmail.com>
2025-07-05 16:44:33 +0800

    初始化项目

---
+++ src/main/java/cn/lwy88098/aiagentdemo/AiAgentDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.lwy88098.aiagentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAgentDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiAgentDemoApplication.class, args);
+    }
+
+}
